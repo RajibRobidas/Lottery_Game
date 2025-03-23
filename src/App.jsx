5cdecd3976bde4a19335290819fb2b4e4da8814d
Rajib <rajibrobidas22222@gmail.com>
2025-03-23 18:11:55 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Ticket from './Ticket';
+import Lottery from './Lottery';
+
+function App() {
+  let winCondition = (ticket) => {
+    return ticket[0] === 0;
+  }
+
+  return (
+    <>
+      <Lottery n={3} winCondition={winCondition} />
+    </>
+  );
+}
+
+export default App;
